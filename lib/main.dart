6ebbc65bca336625c1a3d9c331e8d50dcec3d992
Eric Carlson <e.carlson94@gmail.com>
2023-02-28 04:14:43 +0000

    enable material 3, configure responsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'core/injection/injection_container.dart';
 import 'features/images/presentation/bloc/image_bloc.dart';
-import 'features/images/presentation/widgets/display_image_button.dart';
+import 'features/images/presentation/widgets/display_image.dart';
 import 'features/images/presentation/widgets/pick_image_button.dart';
 
 Future main() async {
@@ -17,14 +18,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'mascot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      builder: (context, child) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, child!),
+        maxWidth: double.infinity,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(450, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+        ],
+      ),
       home: Scaffold(
         body: BlocProvider(
           create: (_) => getIt<ImageBloc>(),
-          child: Column(children: const [
-            PickImageButton(),
-            DisplayImage(),
-          ]),
+          child: Center(
+            child: Column(children: const [
+              PickImageButton(),
+              DisplayImage(),
+            ]),
+          ),
         ),
       ),
     );
