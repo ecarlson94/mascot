55b87ce2c22dc2e8f93cad18be9718b530671218
Eric Carlson <e.carlson94@gmail.com>
2023-03-22 05:44:33 +0000

    fix js interop misconfiguration

--- lib/features/microphone/device/web/web_microphone_impl.dart
+++ lib/features/microphone/device/web/web_microphone_impl.dart
@@ -9,20 +9,27 @@ import '../../../../core/device/web/js_interop/web_audio_js.dart';
 import '../../../../core/utils/logger.dart';
 
 @injectable
-class MascotMicrophoneLogger extends Logger<MascotMicrophone> {}
+class MascotMicrophoneLogger extends Logger<WebMicrophoneImpl> {}
 
-@LazySingleton()
-class MascotMicrophone {
+abstract class WebMicrophone {
+  Future<bool> hasPermission();
+  Stream<double> get volumeStream;
+}
+
+@LazySingleton(as: WebMicrophone)
+class WebMicrophoneImpl implements WebMicrophone {
   final AudioContext _audioContext;
   final MascotMicrophoneLogger _logger;
 
-  MascotMicrophone(this._audioContext, this._logger);
+  WebMicrophoneImpl(this._audioContext, this._logger);
 
+  @override
   Future<bool> hasPermission() async {
     await _setStream();
     return _stream != null;
   }
 
+  @override
   Stream<double> get volumeStream async* {
     final analyzer = await _getAnalyzer();
     final data = Float32List(analyzer.frequencyBinCount);
