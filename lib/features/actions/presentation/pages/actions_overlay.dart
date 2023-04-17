58d374d1558afe98bc2e3f01a80bb9700e2431c1
Eric Carlson <e.carlson94@gmail.com>
2023-04-17 18:41:42 +0000

    add slide transitions to actions overlay

--- lib/features/actions/presentation/pages/actions_overlay.dart
+++ lib/features/actions/presentation/pages/actions_overlay.dart
@@ -6,37 +6,62 @@ import '../../../microphone/presentation/widgets/vertical_loudness_meter/vertica
 import '../../../settings/presentation/bloc/settings_bloc.dart';
 import '../../../settings/presentation/widgets/favorite_mascot_id_provider.dart';
 import '../../../settings/presentation/widgets/talking_threshold_provider.dart';
-import '../widgets/negative_space_toggle.dart';
+import '../widgets/off_screen_slide_transition.dart';
 
-class ActionsOverlay extends StatelessWidget {
+class ActionsOverlay extends StatefulWidget {
   const ActionsOverlay({
     super.key,
   });
 
   @override
+  State<ActionsOverlay> createState() => _ActionsOverlayState();
+}
+
+class _ActionsOverlayState extends State<ActionsOverlay> {
+  bool _showActions = true;
+
+  void _toggleActionVisibility() {
+    setState(() => _showActions = !_showActions);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return NegativeSpaceToggle(
+    const actionPadding = 20.0;
+    return GestureDetector(
+      onTap: _toggleActionVisibility,
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Stack(
           children: <Widget>[
             Positioned(
-              left: 20,
-              bottom: 20,
+              left: actionPadding,
+              bottom: actionPadding,
               child: FavoriteMascotIdProvider(
                 builder: (_, __) => TalkingThresholdProvider(
-                  builder: (_, decibels) => VerticalLoudnessMeter(
-                    sliderThreshold: decibels,
-                    onThresholdChanged: (threshold) => context
-                        .bloc<SettingsBloc>()
-                        .add(SetTalkingThreshold(threshold)),
+                  builder: (_, decibels) => OffScreenSlideTransition(
+                    reverse: !_showActions,
+                    direction: OffScreenSlideDirection.bottomLeft,
+                    child: VerticalLoudnessMeter(
+                      sliderThreshold: decibels,
+                      onThresholdChanged: (threshold) => context
+                          .bloc<SettingsBloc>()
+                          .add(SetTalkingThreshold(threshold)),
+                    ),
                   ),
                 ),
               ),
             ),
+            Positioned(
+              bottom: actionPadding,
+              right: actionPadding,
+              child: OffScreenSlideTransition(
+                reverse: !_showActions,
+                direction: OffScreenSlideDirection.bottomRight,
+                child: const CreateMascotFab(),
+              ),
+            ),
           ],
         ),
-        floatingActionButton: const CreateMascotFab(),
       ),
     );
   }
